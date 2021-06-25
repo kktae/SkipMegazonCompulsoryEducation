829fea24d9039e08c41ed548c00f5f03810f16ea
kktae <82135159+kktae@users.noreply.github.com>
2021-06-25 23:26:24 +0900

    Create MZ_SKIP_EDU.js

---
+++ MZ_SKIP_EDU.js
@@ -0,0 +1,44 @@
+var content = top.contentFrame
+
+var progress = null;
+var btn = null;
+var curPage = null;
+var totalPage = null;
+
+var all = content.document.getElementsByTagName("*");
+for (var i=0, max=all.length; i < max; i++) {
+	progress = content.document.getElementsByClassName("vjs-progress-holder vjs-slider vjs-slider-horizontal")[0]
+	btn = content.document.getElementsByClassName("btn next")[0]
+}
+var nav = content.document.getElementsByClassName("nav");
+for (var i=0, max=nav.length; i < max; i++) {
+	curPage = nav[0].getElementsByClassName("current")[0]
+	totalPage = nav[0].getElementsByClassName("total")[0]
+}
+console.log("find all done")
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+var bLoop = false;
+async function AutoClick() {
+	bLoop = true;
+	while(bLoop) {
+		if(curPage.outerText < totalPage.outerText) {
+			console.log(progress.ariaValueNow)
+			if(progress.ariaValueNow == 100.0) {
+				btn.click()
+				console.log("click!")
+				await sleep(6000)
+			}
+			await sleep(2000)
+		}
+	}
+}
+
+function StopAutoClick() {
+	bLoop = false;
+}
+
+AutoClick();
